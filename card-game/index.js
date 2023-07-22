e0f0be7112ba72372364713878f1c803691dc495
твое имя < email>
2023-07-23 02:04:57 +0300

    add a prettier formatter

---
+++ card-game/index.js
@@ -0,0 +1,53 @@
+"use strict"
+
+const levels = [{ level: 1 }, { level: 2 }, { level: 3 }]
+
+let chosenLevel = null
+const levelList = document.getElementById("levels")
+const startButton = document.querySelector(".start-button")
+
+const renderLevels = () => {
+    const levelHtml = levels
+        .map((level) => {
+            return `<div class="level-item">${level.level}</div>`
+        })
+        .join("")
+
+    levelList.innerHTML = levelHtml
+}
+
+renderLevels()
+
+function chooseLevel() {
+    const selectLevels = document.querySelectorAll(".level-item")
+    for (const selectLevel of selectLevels) {
+        selectLevel.addEventListener("click", (event) => {
+            event.stopPropagation()
+
+            if (chosenLevel !== null) {
+                chosenLevel.classList.remove("chosen-level")
+            }
+            selectLevel.classList.add("chosen-level")
+            chosenLevel = selectLevel
+        })
+    }
+}
+
+chooseLevel()
+
+function startPlaying() {
+    startButton.addEventListener("click", (event) => {
+        event.stopPropagation()
+        if (chosenLevel === null) {
+            alert("Чтобы сыграть, сначала выберите уровень")
+            return
+        }
+
+        const newPage = document.createElement("div")
+        document.body.innerHTML = ""
+        document.body.appendChild(newPage)
+        console.log(`ВАш уровень...`)
+    })
+}
+
+startPlaying()
