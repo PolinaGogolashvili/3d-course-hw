2cf4a2017b9392ccb1d876c34ef48f1d55291547
твое имя < email>
2023-07-22 23:43:32 +0300

    add an choose-level event listener

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
 "use strict";
 
-const levels = [ { level: 1, }, { level: 2, }, { level: 3, }, ];
+const levels = [{ level: 1 }, { level: 2 }, { level: 3 }];
 
-const levelList = document.getElementById('levels');
-const startButton = document.querySelector('.start-button');
+let chosenLevel = null;
+const levelList = document.getElementById("levels");
+const startButton = document.querySelector(".start-button");
 
 const renderLevels = () => {
-    const levelHtml = levels.map((level) => {
-        return `<div class="level-item">${level.level}</div>`;
-      }).join('');
+  const levelHtml = levels
+    .map((level) => {
+      return `<div class="level-item">${level.level}</div>`;
+    })
+    .join("");
 
-      levelList.innerHTML = levelHtml;
-    }
+  levelList.innerHTML = levelHtml;
+};
 
-  renderLevels()
+renderLevels();
+
+function chooseLevel() {
+  const selectLevels = document.querySelectorAll(".level-item");
+  for (const selectLevel of selectLevels) {
+    selectLevel.addEventListener("click", (event) => {
+      event.stopPropagation();
+
+      if (chosenLevel !== null) {
+        chosenLevel.classList.remove("chosen-level");
+      }
+      selectLevel.classList.add("chosen-level");
+      chosenLevel = selectLevel;
+    });
+  }
+}
+
+chooseLevel();
