610090b22b0340fbec24c6d37fbeef3199cc0829
твое имя < email>
2023-07-25 13:11:26 +0300

    add new files

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const CopyPlugin = require("copy-webpack-plugin")
+
+const isProduction = process.env.NODE_ENV === "production"
+
+module.exports = {
+    entry: "./card-game/index.js",
+    mode: isProduction ? "production" : "development",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js",
+        clean: true,
+    },
+    module: {
+        rules: [
+            { test: /\.css$/, use: ["style-loader", "css-loader"] },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: "asset/resource",
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./card-game/index.html",
+        }),
+        new CopyPlugin({
+            patterns: [{ from: "./card-game/static", to: "static" }],
+        }),
+    ],
+}
